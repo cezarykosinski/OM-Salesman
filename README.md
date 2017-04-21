88ea94f42588f7f315438ba0434182c6164da651
Cezary Kosiński <cezary.m.kosinski@gmail.com>
2017-04-21 20:07:35 +0200

    :pig: Sketch for seperating 2-opt and 3-opt implemenation
    [WIP] Waiting for correction

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Travelling Salesman Problem
 ### (Optimalisation Methods course)
+developed by: Elzbieta Plaszczyk & Cezary Kosinski
+all stupid rights reserved 
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int dist(int** dmat, int a, int b)
 {
     return dmat[a][b];
 }
+
 int compute_cost(int** dmat, int* path, int n) {
     int cost = 0;
     for (int i = 1; i < n+1; ++i) {
@@ -22,6 +23,82 @@ void reverse_path (int i, int j, int* path, int n) {
         j--;
     }
 }
+int* 2optTSP(int** dmat, int* path, n, counter){
+    int current_cost = compute_cost(dmat, path, n);
+    char x;
+    bool found_something;
+    int mini, minj, minc, new_cost;
+    do {
+       counter--;
+       minc = current_cost;
+       cout<< "counter " << counter << endl;
+       found_something = false;
+       for (int i = 0; i < n-2; ++i) {
+           for (int j = i+2; j < n-1; ++j) {
+                new_cost = current_cost
+                        - dmat[path[i]][path[i+1]] - dmat[path[j]][path[j+1]]
+                        + dmat[path[i]][path[j]] + dmat[path[i+1]][path[j+1]];
+            //    cout << "moj stary koszt " << current_cost << ", to moj nowy koszt " << new_cost<< endl;
+            //    cout << "miedzy " << path[i] << " i " << path[j]<< endl;
+            //    cin>>x;
+                if (new_cost < minc){
+                    minc = new_cost;
+                    mini = i; minj = j;
+                    found_something = true;
+                }
+           }
+       }
+       if (found_something){
+           current_cost = minc;
+           reverse_path(mini+1, minj, path, n);
+       }
+    }while (found_something && counter > 0);
+    return path;
+}
+
+int* 3optTSP(int** dmat, int* path, n, counter){
+    int current_cost = compute_cost(dmat, path, n);
+    char x;
+    bool found_something;
+    int flow, mini, minj, minc, new_cost;
+    do {
+       flow = 0;
+       counter--;
+       minc = current_cost;
+       found_something = false;
+       for (int i = 0; i < n-5; ++i) {
+           for (int j = i+2; j < n-3; ++j) {
+		   for(int k = j+2; k < n-1; ++k){
+                	new_cost1 = current_cost
+                        	- dmat[path[i]][path[i+1]] - dmat[path[j]][path[j+1]] - dmat[path[k]][path[k+1]]
+                        	+ dmat[path[i]][path[j]] + dmat[path[i+1]][path[j+1]] + ///?????????????????????;
+                	new_cost2 = current_cost
+                        	- dmat[path[i]][path[i+1]] - dmat[path[j]][path[j+1]] - dmat[path[k]][path[k+1]]
+                        	+ dmat[path[i]][path[j]] + dmat[path[i+1]][path[j+1]] + //??????????????????????;
+                	if (new_cost1 < minc){
+                    	    minc = new_cost1;
+                    	    mini = i; minj = j; mink =k;
+                    	    found_something = true;
+		    	    flow = 1;
+                	}
+			if (new_cost2 < minc){
+                    	    minc = new_cost2;
+                    	    mini = i; minj = j; mink = k;
+                    	    found_something = true;
+		    	    flow = 2;
+               		}
+	   	}
+           }
+       }
+       if (found_something){
+           current_cost = minc;
+	   //if flow == 1 bblah blah else halb halb halb
+           reverse_path(mini+1, minj, path, n);
+       }
+    }while (found_something && counter > 0);
+    return path;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -44,42 +121,24 @@ int main(int argc, char *argv[])
         path[i] = i;
     }
     cout<<"Se poczytalem, tera do roboty :)" << endl;
+    // 2opt time counter start
+    int* 2optpath = 2optTSP(dmat, path, n, counter);
+    // time counter stop
+  
+    for (int i = 0; i < n; ++i) {
+   	cout<<2optpath[i]<< " -> ";
+    }
 
-    int current_cost = compute_cost(dmat, path, n);
-    cout<< current_cost<< "WTF" <<endl;
-    char x;
-    bool found_something;
-    int mini, minj, minc, new_cost;
-    do {
-       counter--;
-       minc = current_cost;
-       cout<< "counter " << counter << endl;
-       found_something = false;
-       for (int i = 0; i < n-2; ++i) {
-           for (int j = i+2; j < n-1; ++j) {
-                new_cost = current_cost
-                        - dmat[path[i]][path[i+1]] - dmat[path[j]][path[j+1]]
-                        + dmat[path[i]][path[j]] + dmat[path[i+1]][path[j+1]];
-                cout << "moj stary koszt " << current_cost << ", to moj nowy koszt " << new_cost<< endl;
-                cout << "miedzy " << path[i] << " i " << path[j]<< endl;
-            //    cin>>x;
-                if (new_cost < minc){
-                    minc = new_cost;
-                    mini = i; minj = j;
-                    found_something = true;
-                }
-           }
-       }
-       if (found_something){
-           current_cost = minc;
-           reverse_path(mini+1, minj, path, n);
-           //swap(path[mini],path[minj+1]);
-       }
-       for (int i = 0; i < n; ++i) {
-           cout<<path[i]<< " -> ";
-       }
-       cin>>x;
-       cout<<endl;
-    }while (found_something && counter > 0);
+    cout<<endl;
+    // 3opt time counter start
+    int* 3optpath = 3optTSP(dmat, path, n, counter);
+    // time counter stop
+
+    for (int i = 0; i < n; ++i) {
+   	cout<<3optpath[i]<< " -> ";
+    }
+    cout<<endl;
+
+
     return 0;
 }
